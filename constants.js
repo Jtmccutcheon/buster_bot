7bf8df6b67f95d0732f55f29449f331493295638
unknown <jt.mccutcheon@live.com>
2022-02-22 03:27:06 -0500

    update constants

--- constants.js
+++ constants.js
@@ -1,8 +1,8 @@
 const CRON_EXPRESSIONS = {
   botd: '9 18 * * *', // At 06:09 PM everday
-  backup: '11 18 * * *', // At 06:12 PM everday
+  backup: '11 18 * * *', // At 06:11 PM everday
   botm: '50 9 18 28-31 * *', // At 06:09:50 PM, between day 28 and 31 of the month
-  boty: '15 18 31 12 *', // At 06:15 PM, on day 31 of the month, only in December
+  boty: '12 18 31 12 *', // At 06:12 PM, on day 31 of the month, only in December
   debug: '16 23 * * *',
 };
 
